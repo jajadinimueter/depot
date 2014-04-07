3b23a9387b7f9be4c5f0ce9615ed402dfdab9aa7
Florian Müller <jajadinimueter@gmail.com>
2014-04-07 23:38:29 +0200

    ShoppingCartErweitern (Chapter 9: Cart Creation)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,11 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+
+	def get_total_price				# Übung ShoppingCartErweitern (Kap. 9)
+        price = 0
+        line_items.each do |line_item|
+           	price += line_item.product.price
+        end
+    	return price 
+    end
 end
