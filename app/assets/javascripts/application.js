aa660875986cc1927605d4339f75f09713a119a8
Florian Müller <jajadinimueter@gmail.com>
2014-05-27 16:18:17 +0200

    Iteration F3: Highlighting Changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+// Iteration F3 (Chapter 11.3, p. 147), following line
+//= require jquery.ui.effect-blind
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
