66c87a551f21c51e879d0b6da7c5fbaaee0ebdec
Florian Müller <jajadinimueter@gmail.com>
2014-06-09 12:28:11 +0200

    Iteration J2: Translating the Storefront (number_to_chf replaced by de-ch)

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,5 +3,6 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'],
-  ["Espa&ntilde;ol".html_safe, 'es']
+  ["Espa&ntilde;ol".html_safe, 'es'],
+  ['Deutsch (Schweiz)',     'de-ch'],	# Iteration J2 (Chapter 15.2, p. 222)
 ]
