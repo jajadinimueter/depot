f467dfafa0e78c92a659b8393ad816ab839cbcc7
Florian Müller <jajadinimueter@gmail.com>
2014-06-09 14:50:11 +0200

    Iteration J4: Add a Locale Switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,14 @@
 class StoreController < ApplicationController
   include CurrentCart		# Iteration F1, Chapter 11.1, p. 139
   before_action :set_cart
-  def index
-  	@products = Product.order(:title)
-  end
+  # def index
+  # 	@products = Product.order(:title)
+  # end
+  def index			# Iteration J4 (Chapter 15.4, p. 229)
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
+  end  
 end
