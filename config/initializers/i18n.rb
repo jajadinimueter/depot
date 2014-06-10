b1780304fc9c96fade8e9b37f2b11b9ba083ff51
Florian Müller <jajadinimueter@gmail.com>
2014-06-10 04:09:21 -0700

    Fixed some issues

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,7 +3,5 @@ I18n.default_locale = :en
 
 LANGUAGES = [
   ['English',                  'en'],
-  ["Espa&ntilde;ol".html_safe, 'es'],
-  ['Deutsch (Schweiz)',     'de-ch'],	# Iteration J2 (Chapter 15.2, p. 222)
-  ['Deutsch (Deutschland)', 'de-de'],
+  ['Deutsch (Schweiz)',     'de-ch']
 ]
